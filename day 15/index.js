a5f84ae33ae284c5c6a4cbbd7991d979cdc1249b
sahiinkhatri <jungey20004@gmail.com>
2023-09-06 17:46:49 +0545

    day 15

---
+++ day 15/index.js
@@ -0,0 +1,70 @@
+//let marks =prompt("enter your marks")
+//if(marks>=80){
+  //  console.log("you have got distinction");
+    
+//}else if  (marks>=70){
+  //  console.log("first division");
+//}else if  (marks>=60){
+  //  console.log("second division");
+//}else if  (marks>=40){
+    //console.log("third dividion");
+//}else {
+  //  console.log("fail");
+//}
+//if(marks%2){
+//    console.log("your marks is even");
+//} else{
+  //  console.log("your marks is odd");
+
+//}
+
+
+
+
+
+
+// const marks=prompt(`enter your marks here`);
+
+// const num = Number(marks);
+ 
+// let message;
+// if(num%2==0){
+//     message="even";
+// }else {
+//     message="odd";
+// }
+
+
+// if(num>=80 && num <=100){
+//     console.log(`marks ` + num+ ` is `+ message + ` And It is distinction`);
+// }else  if(num>=70 && num < 80) {
+//     console.log(`marks ` + num+ ` is `+ message + ` And It isfirst division`);
+// }else  if(num>=65 && num < 70) {
+//     console.log(`marks ` + num+ ` is `+ message + ` And It is second division`);
+// }else  if(num>=40 && num < 60) {
+//     console.log(`marks ` + num+ ` is `+ message + ` And It is third division`);
+// }else  if(num>=0 && num < 40) {
+//     console.log(`marks ` + num+ ` is `+ message + ` And It is fail`);
+// }
+// else {
+//     console.write(`marks doesnt exist`)
+// }
+
+
+
+
+
+
+
+function sum(q) {
+    let x=10;
+    let y=21;
+    let z=21;
+    let q= (x+y+z);
+}
+console.log (q);
+
+sum(q);
+
+
+
